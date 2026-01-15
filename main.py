2c7485584712f0c265911192323ef5533aeeffd2
theoldestdream1 <ditakaima00@gmail.com>
2026-01-15 12:13:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -109,5 +109,7 @@ async def get_stats():
 
 
 if __name__ == "__main__":
+    import os
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=12000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=8000)
